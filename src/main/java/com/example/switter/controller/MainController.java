22a4f2940280e58e5936d6070fe0fdb2f659d0e8
Erasyl97 <era.berikbol@gmail.com>
2020-05-01 22:49:02 +0600

    feature Lesson4 added connections between db columns.ManyToOne

--- src/main/java/com/example/switter/controller/MainController.java
+++ src/main/java/com/example/switter/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.switter.controller;
 
 import com.example.switter.domain.Message;
+import com.example.switter.domain.User;
 import com.example.switter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,8 +32,11 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        messageRepo.save(new Message(text, tag));
+    public String add(@AuthenticationPrincipal User user,
+                      @RequestParam String text,
+                      @RequestParam String tag,
+                      Map<String, Object> model) {
+        messageRepo.save(new Message(text, tag, user));
 
         Iterable<Message> messages = messageRepo.findAll();
 
